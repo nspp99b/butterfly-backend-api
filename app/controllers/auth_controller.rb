13150e7dfa899537e38c22429c18ee9ac9bade5d
nspp99b <chromepanthers@gmail.com>
2018-02-21 17:49:00 -0500

    fixed auth controller login action, added routes and controller actions for logout, signup, currentUser

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,12 +1,52 @@
 class AuthController < ApplicationController
 
   def login
-    u = User.find_by(username: params[:username])
+    u = User.find_by(email: params[:email])
     if u && u.authenticate(params[:password])
       token = issue_token({ 'user_id': u.id })
       render json: {'token': token }
     else
-      render json: {'error': 'Incorrect Username or Password'}, status: 401
+      render json: {'error': 'Incorrect Email or Password'}, status: 401
     end
   end
+
+  def logout
+    user = current_user
+    @@all.delete_if {|u| u.id === user.id }
+    render json: nil
+  end
+
+  def currentUser
+    user = current_user
+    if user
+      render json: { id: user.id, email: user.email }
+    else
+      render json: nil
+    end
+  end
+
+  def signup
+    user = User.new(user_params)
+    if user.save
+      begin
+        user = login_user(user_params[:email], user_params[:password])
+        render json: {
+          id: user.id,
+          email: user.email,
+          token: encode_token({'user_id': user.id})
+        }
+      rescue AuthError => e
+        render json: { error: e.msg }, status: 401
+      end
+    else
+      render json: nil
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   post "/login", to: 'auth#login'
+  post '/logout', to: 'auth#logout'
+  get '/current_user', to: 'auth#currentUser'
+  post '/signup', to: 'auth#signup'
 
 end
