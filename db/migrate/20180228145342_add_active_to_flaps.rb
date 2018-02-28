e592d8f7d4c6cc36e3cfc2e5c3963bfa015925b9
nspp99b <chromepanthers@gmail.com>
2018-02-28 10:03:02 -0500

    added active boolean column to flaps

---
+++ db/migrate/20180228145342_add_active_to_flaps.rb
@@ -0,0 +1,5 @@
+class AddActiveToFlaps < ActiveRecord::Migration[5.1]
+  def change
+    add_column :flaps, :active, :boolean, :default => true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180221204659) do
+ActiveRecord::Schema.define(version: 20180228145342) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20180221204659) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "active", default: true
     t.index ["user_id", "created_at"], name: "index_flaps_on_user_id_and_created_at"
   end
 
