940990f9a1dd332874d0abb8d4e546867992cb1f
nspp99b <chromepanthers@gmail.com>
2018-02-28 10:40:30 -0500

    added update route to flaps resource

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :show]
-      resources :flaps, only: [:index, :create, :show, :destroy]
+      resources :flaps, only: [:index, :create, :show, :update]
       resources :relationships, only: [:index, :create]
     end
   end
