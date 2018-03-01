ccb4753be5dae28ad476aa28cc16cd7b9b4e6861
nspp99b <chromepanthers@gmail.com>
2018-03-01 16:41:11 -0500

    debugged user show route

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def show
-    user = User.find(params[:id]).to_json
+    user = User.find(params[:id])
     flaps = user.flaps.order(created_at: :desc).map { |f| f.to_json }
     render json: { user: user, flaps: flaps }
   end
